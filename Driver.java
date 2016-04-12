400558ab18a5655d454688143f42d7ec08811b8a
jtf28 <jtf28@pitt.edu>
2016-04-11 23:11:55 -0400

    very rough skeleton of Driver

---
+++ Driver.java
@@ -0,0 +1,67 @@
+public class Driver
+{
+    public static void main( String args[] )
+    {
+        
+    }
+
+    public boolean createUser()
+    {
+        return false;
+    }
+
+    public boolean initiateFriendship()
+    {
+        return false;
+    }
+
+    public boolean establishFriendship()
+    {
+        return false;
+    }
+
+    public boolean displayFriends()
+    {
+        return false;
+    }
+
+    public boolean createGroup()
+    {
+        return false;
+    }
+
+    public boolean addToGroup()
+    {
+        return false;
+    }
+
+    public boolean sendMessageToUser()
+    {
+        return false;
+    }
+
+    public boolean displayMessages()
+    {
+        return false;
+    }
+
+    public boolean searchForUser()
+    {
+        return false;
+    }
+
+    public boolean threeDegrees()
+    {
+        return false;
+    }
+
+    public boolean topMessagers()
+    {
+        return false;
+    }
+
+    public boolean dropUser()
+    {
+        return false;
+    }
+}
